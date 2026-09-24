6fe86c64ebcfe393588ecc2427cf82e7877d872c
Demetreius <tchendjedimitri@gmail.com>
2026-09-24 11:45:23 +0200

    feat ( pipes, interceptor ): Introducing the pipes and interceptor in the app

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { LoggerService } from './user.logger.js';
 import { CreateUserDto } from './dto/create-user.dto.js';
 import { UpdateUserDto } from './dto/update-user.dto.js';
